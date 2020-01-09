d795d8b95defd8b26ccf8d593798bc84b5253fcf
Justin L Ross <37995778+justinlross@users.noreply.github.com>
2020-01-09 18:46:26 +1000

    Add files via upload

--- house_prices_advanced_regression_techniques.py
+++ house_prices_advanced_regression_techniques.py
@@ -5,6 +5,7 @@ Created on Wed Apr  3 09:37:59 2019
 @author: Justin L Ross
 """
 
+######################################################################
 # Load libraries
 # data analysis and wrangling
 import pandas as pd
@@ -26,11 +27,52 @@ from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 
+######################################################################
+# Declare variables and constants
 # Load data, train_df is the training data and test_df is the testing data.
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
 combine = [train_df, test_df]
 
+######################################################################
+# Functions and code.
+
+# Prelimnary Analysis
+
+train_df.head()
+train_df.describe()
+train_df.shape
+train_df.keys()
+
+correlations = train_df.corr()
+correlations = correlations["SalePrice"].sort_values(ascending=False)
+features = correlations.index[1:6]
+correlations
+
+training_null = pd.isnull(train_df).sum()
+testing_null = pd.isnull(test_df).sum()
+
+
+# Dealing with null vaues
+
+null = pd.concat([training_null, testing_null], axis=1, keys=["Training", "Testing"])
+
+null_many = null[null.sum(axis=1) > 200]  #a lot of missing values
+null_few = null[(null.sum(axis=1) > 0) & (null.sum(axis=1) < 200)]  #not as much missing values
+
+null_many
+
+#you can find these features on the description data file provided
+
+null_has_meaning = ["Alley", "BsmtQual", "BsmtCond", "BsmtExposure", "BsmtFinType1", "BsmtFinType2", "FireplaceQu", "GarageType", "GarageFinish", "GarageQual", "GarageCond", "PoolQC", "Fence", "MiscFeature"]
+
+
+for i in null_has_meaning:
+    training[i].fillna("None", inplace=True)
+    testing[i].fillna("None", inplace=True)
+    
+#### MORE HERE ####  
+    
 # Random Forest
 
 y = train_df["SalePrice"]
@@ -46,4 +88,10 @@ predictions = model.predict(X_test)
 output = pd.DataFrame({'Id': test_df.Id, 'SalePrice': predictions})
 output.to_csv('my_submission.csv', index=False)
 print("Your submission was successfully saved!")
+
+
+######################################################################
+# Functions to excude and run.
+
+
 
